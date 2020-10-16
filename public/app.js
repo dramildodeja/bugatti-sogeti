763b81ea7cf99a9df9960998e51e902ec9ff28a8
DD <dodeja_dramil@hotmail.com>
2020-10-15 23:45:21 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("realtimedataanalyzer",[]);
+var app = angular.module("searchops",[]);
 app.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
 }]);
